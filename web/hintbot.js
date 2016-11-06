2037113d4019b0568fceddb73065df645708175e
Ollin Boer Bohan <madebyollin@gmail.com>
2016-11-06 13:49:07 -0800

    rough implementation of prediction (just prints to console)

--- web/hintbot.js
+++ web/hintbot.js
@@ -14,23 +14,35 @@ function init() {
     hintbot.ready().then(() => {
         document.getElementById("spinner").classList.add("hidden");
         document.getElementById("wrapper").classList.remove("hidden");
-        // input data object keyed by names of the input layers
-        // or `input` for Sequential models
-        // values are the flattened Float32Array data
-        // (input tensor shapes are specified in the model config)
-        const inputData = {
-            'input_1': new Float32Array(data)
-        }
+        document.getElementById("fileInput").addEventListener("change", function() {
+            var reader = new FileReader();
+            reader.onload = function() {
+                var array = new Uint8Array(this.result),
+                console.log(array);
 
-        // make predictions
-        // outputData is an object keyed by names of the output layers
-        // or `output` for Sequential models
-        model.predict(inputData).then(outputData => {
-            // e.g.,
-            // outputData['fc1000']
-        }).catch(err => {
-            // handle error
-        });
+                // input data object keyed by names of the input layers
+                // or `input` for Sequential models
+                // values are the flattened Float32Array data
+                // (input tensor shapes are specified in the model config)
+                const inputData = {
+                    'input_1': array
+                }
+
+                // make predictions
+                // outputData is an object keyed by names of the output layers
+                // or `output` for Sequential models
+                hintbot.predict(inputData).then(outputData => {
+                    // e.g.,
+                    // outputData['fc1000']
+                    console.log("Model output:");
+                    console.log(JSON.stringify(outputData.output, null, 4));
+                }).catch(err => {
+                    console.log(err);
+                });
+            }
+
+            reader.readAsArrayBuffer(this.files[0]);
+        }, false);
     });
 }
 window.onload = init;
