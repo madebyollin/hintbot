230e0214f68730d3b2813f9169d88c2c791fc29f
Ollin Boer Bohan <madebyollin@gmail.com>
2016-11-06 13:39:43 -0800

    started work on website

---
+++ web/hintbot.js
@@ -0,0 +1,36 @@
+function loadHintBot() {
+    return new KerasJS.Model({
+        filepaths: {
+            model: 'web/hintbot.json',
+            weights: 'web/hintbot.buf',
+            metadata: 'web/hintbot_metadata.json'
+        },
+        gpu: true
+    })
+}
+
+function init() {
+    hintbot = loadHintBot();
+    hintbot.ready().then(() => {
+        document.getElementById("spinner").addClass("hidden");
+        document.getElementById("wrapper").removeClass("hidden");
+        // input data object keyed by names of the input layers
+        // or `input` for Sequential models
+        // values are the flattened Float32Array data
+        // (input tensor shapes are specified in the model config)
+        const inputData = {
+            'input_1': new Float32Array(data)
+        }
+
+        // make predictions
+        // outputData is an object keyed by names of the output layers
+        // or `output` for Sequential models
+        model.predict(inputData).then(outputData => {
+            // e.g.,
+            // outputData['fc1000']
+        }).catch(err => {
+            // handle error
+        });
+    });
+}
+window.onload = init;
